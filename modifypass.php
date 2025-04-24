bb5a2802afe9b98b96322b7a24bd776a3be5c697
NatCode <NatCode171@gmail.com>
2025-04-24 11:19:22 +0200

    Update modifypass.php

--- modifypass.php
+++ modifypass.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'init.php';
+
+$url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
+$url .= "://".$_SERVER['HTTP_HOST']; // si on est sur dev. ou pas
+
+$target_user_id = isset($_GET['id']) ? $_GET['id'] : '';
+$target_user_id_code_email = isset($_GET['idemail']) ? $_GET['idemail'] : '';
 ?>
 
 <!DOCTYPE html>
@@ -27,83 +33,88 @@ require_once 'init.php';
         ?>
         <div class="main">
             <div class="forgotpassword">
-                <form method="POST" action="modifypass" enctype="multipart/form-data" id="forgotpassword-form">
+                <form method="POST" action="<?php echo "$url/modifypass?id=$target_user_id&idemail=$target_user_id_code_email"; ?>" enctype="multipart/form-data" id="forgotpassword-form">
                     <h1>Mot de passe oublié ?</h1>
                     <br>
                     <?php
-                    $target_user_id = isset($_GET['id']) ? $_GET['id'] : '';
-                    $target_user_id_code_email = isset($_GET['idemail']) ? $_GET['idemail'] : '';
-                    if ($target_user_id !== '' && $target_user_id_code_email !== '') {
+                    if ($target_user_id != '' && $target_user_id_code_email != '') {
 
                         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                             $targetEmail = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
-                            $newpassword = $_POST['password'];
-                            
-                            $targetCode =(int)$_POST['code'];
-                                
-                            if (filter_var($targetEmail, FILTER_VALIDATE_EMAIL)) {
-                                $targetEmail = trim(strtolower($targetEmail));
+                            $newpassword1 = $_POST['password1'];
+                            $newpassword2 = $_POST['password2'];
 
-                                $sql = $pdo->prepare("SELECT id, ban FROM users WHERE email = ?");
-                                $sql->execute([$targetEmail]);
-                                $result = $sql->fetch(PDO::FETCH_ASSOC);
+                            if ($newpassword1 == $newpassword2) {
 
-                                if ($result) {
-                                    $targetUser_id = (int)$result['id'];
-                                    if ((int)$result['ban']) {
-                                        die("<div class='alert'><strong>Vous avez été banni de Project Sharing, voici la ou les raison(s) :</strong><br><br>$ban_reason</div>");
-                                    } else {
-                                        if ($targetUser_id != $target_user_id) {
-                                            $forgotpasswordErrors = "<div class='alert'><p>Email non valide.</p></div>";
+                                $targetCode =(int)$_POST['code'];
+                                    
+                                if (filter_var($targetEmail, FILTER_VALIDATE_EMAIL)) {
+                                    $targetEmail = trim(strtolower($targetEmail));
+
+                                    $sql = $pdo->prepare("SELECT id, ban, ban_reason FROM users WHERE email = ?");
+                                    $sql->execute([$targetEmail]);
+                                    $result = $sql->fetch(PDO::FETCH_ASSOC);
+
+                                    if ($result) {
+                                        $targetUser_id = (int)$result['id'];
+                                        if ((int)$result['ban']) {
+                                            $ban_reason = htmlspecialchars($result['ban_reason']);
+                                            $errorautre = "<div class='alert'><strong>Vous avez été banni de Project Sharing, voici la ou les raison(s) :</strong><br><br>$ban_reason</div>";
                                         } else {
-                                            $sql = $pdo->prepare("SELECT nbpass, nbpassemail FROM modifypass WHERE email = ?");
-                                            $sql->execute([$targetEmail]);
-                                            $result = $sql->fetch(PDO::FETCH_ASSOC);
+                                            if ($targetUser_id != $target_user_id) {
+                                                $forgotpasswordErrors = "<div class='alert'>$targetUser_id != $target_user_id</div>";
+                                            } else {
+                                                $sql = $pdo->prepare("SELECT nbpass, nbpassemail, timestamp FROM modifypass WHERE email = ?");
+                                                $sql->execute([$targetEmail]);
+                                                $result = $sql->fetch(PDO::FETCH_ASSOC);
 
-                                            if ($result) {
-                                                $nbpass = (int)$result['nbpass'];
-                                                $nbpassemail = (int)$result['nbpassemail'];
-
-                                                if ($nbpass == $targetCode && $nbpassemail == $target_user_id_code_email) {
-
+                                                if ($result) {
+                                                    $nbpass = (int)$result['nbpass'];
+                                                    $nbpassemail = (int)$result['nbpassemail'];
+                                                    $lastAttempt = strtotime($result['timestamp']);
                                                     $currentTime = time();
-                                                    $lastAttempt = strtotime($result['timestamp']);                         
 
-                                                    if ($currentTime - $lastAttempt <= 60 * 15) {
-                                                        $remainingTime = 60 * 15 - floor(($currentTime - $lastAttempt) / 60);
-                                                        $forgotpasswordErrors = "<div class='alert'>Délais dépassé.";
-                                                    } else {
-                                                        if (strlen($password) < 8 || 
-                                                            !preg_match('/\d/', $password) || 
-                                                            !preg_match('/[a-z]/', $password) || 
-                                                            !preg_match('/[A-Z]/', $password) || 
-                                                            !preg_match('/[\W_]/', $password)) {
-                                                            $codeErrorspassword['password'] = "<div class='alert'>Votre mot de passe doit contenir au moins 8 caractères, incluant une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.</div>";
+                                                    if ($nbpass == $targetCode && $nbpassemail == $target_user_id_code_email) {
+
+                                                        if ($currentTime - $lastAttempt >= 60 * 15) {
+                                                            $forgotpasswordErrors = "<div class='alert'>Délais dépassé.</div>";
                                                         } else {
-                                                            $new_password_hashed = password_hash($new_password, PASSWORD_DEFAULT);
-
-                                                            $update_query = "UPDATE users SET password = ? WHERE id = ?";
-                                                            $stmt_update = $pdo->prepare($update_query);
-                                                            if ($stmt_update->execute([$new_password_hashed, $target_user_id])) {
-                                                                header('Location: /');
-                                                                exit();
+                                                            if (strlen($newpassword1) < 8 || 
+                                                                !preg_match('/\d/', $newpassword1) || 
+                                                                !preg_match('/[a-z]/', $newpassword1) || 
+                                                                !preg_match('/[A-Z]/', $newpassword1) || 
+                                                                !preg_match('/[\W_]/', $newpassword1)) {
+                                                                $codeErrorspassword = "<div class='alert'>Votre mot de passe doit contenir au moins 8 caractères, incluant une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.</div>";
                                                             } else {
-                                                                $codeErrorspassword['password'] = "<div class='alert'><p>Erreur lors de la mise à jour.</p></div>";
+                                                                $new_password_hashed = password_hash($newpassword1, PASSWORD_DEFAULT);
+
+                                                                $update_query = "UPDATE users SET password = ? WHERE id = ?";
+                                                                $stmt_update = $pdo->prepare($update_query);
+                                                                if ($stmt_update->execute([$new_password_hashed, $target_user_id])) {
+                                                                    header('Location: /login');
+                                                                    exit();
+                                                                } else {
+                                                                    $codeErrorspassword = "<div class='alert'><p>Erreur lors de la mise à jour.</p></div>";
+                                                                }
                                                             }
                                                         }
+                                                    } else {
+                                                        $codeErrors = "<div class='alert'>Code incorecte ou vous êtes sur la mauvais lien.</div>";
                                                     }
+                                                } else {
+                                                    $forgotpasswordErrors = "<div class='alert'>Vous n'avez pas fais de demmande de modification de mot de passe.</div>";
                                                 }
-                                            } else {
-                                                $forgotpasswordErrors = "<div class='alert'>Email non valide.</div>";
                                             }
                                         }
+                                    } else {
+                                        $forgotpasswordErrors = "<div class='alert'><p>Cette email n'est pas enregistrée sur notre site.</p></div>";
                                     }
                                 } else {
                                     $forgotpasswordErrors = "<div class='alert'><p>Email non valide.</p></div>";
                                 }
                             } else {
-                                $forgotpasswordErrors = "<div class='alert'><p>Email non valide.</p></div>";
+                                $codeErrorspassword = "<div class='alert'><p>Les mots de passe ne coresspondent pas.</p></div>";
                             }
                         }
                     }
@@ -120,11 +131,19 @@ require_once 'init.php';
                     <?php if (isset($codeErrors)) { echo $codeErrors . "<br>"; } ?>
                     <br>
 
-                    <label for="password">Nouveau mot de passe :</label>
+                    <label for="password1">Nouveau mot de passe :</label>
                     <br>
-                    <input type="text" id="password" name="password" required value="<?php echo isset($_POST['password']) ? $_POST['password'] : ''; ?>">
+                    <input type="password" id="password1" name="password1" required>
                     <?php if (isset($codeErrorspassword)) { echo $codeErrorspassword . "<br>"; } ?>
                     <br>
+
+                    <label for="password2">Confirmer le mot de passe :</label>
+                    <br>
+                    <input type="password" id="password2" name="password2" required>
+                    <?php if (isset($codeErrorspassword)) { echo $codeErrorspassword . "<br>"; } ?>
+                    <br>
+
+                    <?php if (isset($errorautre)) { echo $errorautre . "<br>"; } ?>
                     <button type="submit">Envoyer</button>
                 </form>
             </div>
